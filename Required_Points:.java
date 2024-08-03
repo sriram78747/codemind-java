d482515f7e7e33fa88f25df967d32f0c21f35c3b
sriram78747 <147419201+sriram78747@users.noreply.github.com>
2024-08-03 11:48:12 +0530

    import java.util.Scanner; public class points{     public static void main(String[] args){         int a,b,c;         Scanner read = new Scanner(System.in);         a = read.nextInt();         b = read.nextInt();         c = b/a;         System.out.println(c);     } }

---
+++ Required_Points:.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class points{
+    public static void main(String[] args){
+        int a,b,c;
+        Scanner read = new Scanner(System.in);
+        a = read.nextInt();
+        b = read.nextInt();
+        c = b/a;
+        System.out.println(c);
+    }
+}
