8a4076d4cf46ebdb0619226920561311c0424362
sriram78747 <147419201+sriram78747@users.noreply.github.com>
2024-08-03 11:57:05 +0530

    import java.util.*; public class km{     public static void main(String[] args){         int a;         double b;         Scanner read = new Scanner(System.in);         a = read.nextInt();         b = (a*5)/18.0;         System.out.printf("%.2f",b);     } }

---
+++ kmph_to_mps.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class km{
+    public static void main(String[] args){
+        int a;
+        double b;
+        Scanner read = new Scanner(System.in);
+        a = read.nextInt();
+        b = (a*5)/18.0;
+        System.out.printf("%.2f",b);
+    }
+}
